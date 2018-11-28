2615c127761927b6d66010005aa69f80b8efcb70
Juan Sifontez <juansfontz@gmail.com>
2018-11-28 12:43:46 -0400

    Fix URL deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:100DaysOfVue/tic-tac-toe/dist/index.html.git master:gh-pages
+git push -f git@github.com:100DaysOfVue/tic-tac-toe.git master:gh-pages
 
 cd -
